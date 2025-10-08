3673e6115f57d4e51ebec948ae7e6aae4bb96ec6
iaconversas.of <iaconversas.of@gmail.com>
2025-10-08 14:32:07 -0300

    Feat: Redireciona a rota raiz para /admin.

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SyncProgressController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
 
 // Rotas para progresso da sincronização
